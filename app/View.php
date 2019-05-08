0f22fef70ba044a63ffd73f5789f526ef2b1ab94
dmarostega <dmarostega@gmail.com>
2019-05-08 14:03:53 -0300

    Iniciando Criação Classes View, Link e Script

--- app/View.php
+++ app/View.php
@@ -1,10 +1,38 @@
 <?php
 
+
+class Link{
+    private $as;
+    private $charset;
+    /*  HTML5 */
+    private $crossorigin;
+    private $disabled = false;
+    private $href;
+    private $hreflnag;
+    private $media;
+    private $methods;
+    private $rel;
+    /*  HTML5 */
+    private $sizes;
+}
+
+class Script{
+    /*  HTML5 */
+    private $async;
+    private $defer;
+    private $integrity;
+    private $nomodule;
+    private $nonce;
+    private $referrerpolicy;
+    private $src;
+    private $type;
+    private $charset;
+    private $language;
+}
+
 class View {
     private $css;
     private $js;
-    private $template;
+    private $template = 'default.html';
     private $data;
-    
-    
 }
